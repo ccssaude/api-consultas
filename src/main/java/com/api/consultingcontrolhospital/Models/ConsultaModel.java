3cc857d240b0a9ab90f3a367648e696ef14835a5
ccsaude <agnaldosamuel@ccsaude.org.mz>
2022-06-30 18:01:24 -0400

    Making relationship OneToOne between paciente & consulta

---
+++ src/main/java/com/api/consultingcontrolhospital/Models/ConsultaModel.java
@@ -0,0 +1,50 @@
+package com.api.consultingcontrolhospital.Models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="TB_CONSULTA")
+public class ConsultaModel implements Serializable {
+
+
+    private static final long serialVersionUID =1L;
+    @Id
+    @Column(name = "consulta_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, length = 25)
+    private String nome;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private PacienteModel;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
